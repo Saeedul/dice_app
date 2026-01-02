9e05fc17f9d43637ff318e7cc9262574365558c3
Saeedul <karimsaeedul476@gmail.com>
2026-01-02 18:36:50 +0600

    updated

--- lib/screens/my_home_screen.dart
+++ lib/screens/my_home_screen.dart
@@ -10,6 +10,36 @@ class MyHomeScreen extends StatefulWidget {
 class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.green.shade500,
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: const Text("Dice App"),
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset("assets/dice2.png"),
+                ),
+              ),
+            ),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset("assets/dice3.png"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
